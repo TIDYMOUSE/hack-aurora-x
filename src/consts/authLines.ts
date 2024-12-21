47a6c14009566b297361cb25bdfc921a6342ac14
Warren Jacinto <warrenjacinto@gmail.com>
2024-12-21 19:54:47 +0530

    consts

---
+++ src/consts/authLines.ts
@@ -0,0 +1,4 @@
+export const authLines = {
+    description: "This is the Login/Signup page, where you can enter your details to login or signup to the website"
+
+}
---
+++ src/consts/transactionsLines.ts
@@ -0,0 +1,5 @@
+export const transactionsLines = {
+    "description": "This page provides an overview of your past history of transactions",
+    "possible_navs": "You can go to the dashboard, budgets, accounts, investments, meta, stats pages",
+    
+}
